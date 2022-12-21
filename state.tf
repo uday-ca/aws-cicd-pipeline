bd4d0027b35df345ca548abdacccc01df6544b50
Uk <udayakumar.kolathur@gmail.com>
2022-12-21 17:32:21 -0500

    new test lambday

--- state.tf
+++ state.tf
@@ -9,10 +9,6 @@ terraform {
 
 terraform {
   required_providers {
-    aws = {
-        source = "hashicorp/aws"
-        version = "4.36.1"
-    }
     archive = {
       source  = "hashicorp/archive"
       version = "~> 2.2.0"
