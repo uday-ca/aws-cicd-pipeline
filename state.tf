297939a8210bfa7042c30426f2d6d3225719e990
Uk <udayakumar.kolathur@gmail.com>
2022-12-21 17:28:40 -0500

    new test lambda

--- state.tf
+++ state.tf
@@ -6,6 +6,21 @@ terraform {
         region = "us-east-1"
     }
 }
+
+terraform {
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "4.36.1"
+    }
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 2.2.0"
+    }
+  }
+  required_version = "~> 1.0"
+}
+
 provider "aws" {
     region = "us-east-1"
 }
