fed15fcc6c9dae66071a0606318500dfcf74d4e1
Uk <udayakumar.kolathur@gmail.com>
2022-12-21 18:16:41 -0500

    lmabdatest

--- state.tf
+++ state.tf
@@ -6,6 +6,15 @@ terraform {
         region = "us-east-1"
     }
 }
+terraform {
+  required_providers {
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 2.2.0"
+    }
+  }
+  required_version = "~> 1.0"
+}
 provider "aws" {
     region = "us-east-1"
 }
