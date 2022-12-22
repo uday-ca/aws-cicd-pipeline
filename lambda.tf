c39be2266a66da2d06347cfef0e71dca5b6cfbaa
Uk <udayakumar.kolathur@gmail.com>
2022-12-22 01:49:48 -0500

    testee

--- lambda.tf
+++ lambda.tf
@@ -1,43 +1,8 @@
-resource "aws_iam_role" "Trz_lambda_role" {
-    name = "Trz_lambda_role"
-    assume_role_policy = jsonencode({
-        "Version": "2012-10-17",
-        "Statement": [{
-            "Effect": "Allow",
-            "Principal": {
-                "Service": "lambda.amazonaws.com"
-            },
-            "Action": "sts:AssumeRole"
-        }]
-    }) 
-}
+import json
 
-resource "aws_iam_policy_attachment" "Trz_role_lambda_policy_attachment" {
-    name = "Trz_role_lambda_policy_attachment"
-    roles = [ aws_iam_role.Trz_lambda_role.name ]
-    policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-}
-
-data "archive_file" "Trz_lambda_source_archive" {
-  type = "zip"
-
-  source_dir  = "${path.module}/src"
-  output_path = "${path.module}/my-deployment.zip"
-}
-resource "aws_lambda_function" "Trz_lambda-cicd" {
-    function_name = "Trz_lambda-cicd"
-    filename = "${path.module}/my-deployment.zip"
-
-    runtime = "python3.9"
-    handler = "lambda_function.lambda_handler"
-    memory_size = 256
-
-    source_code_hash = data.archive_file.Trz_lambda_source_archive.output_base64sha256
-
-    role = aws_iam_role.Trz_lambda_role.arn
-}
-resource "aws_cloudwatch_log_group" "Trz_lambda-cicd" {
-  name = "/aws/lambda/${aws_lambda_function.Trz_lambda-cicd.function_name}"
-
-  retention_in_days = 1
-}
+def lambda_handler(event, context):
+    # TODO implement
+    return {
+        'statusCode': 200,
+        'body': json.dumps('UK123-Hello from Lambda!-PostDeploymentUpdate')
+    }
