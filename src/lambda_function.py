afce384b6616f113e01647843052c99ee07eef0d
Uk <udayakumar.kolathur@gmail.com>
2022-12-22 13:59:03 -0500

    checkin trigger cicd

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -4,5 +4,5 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('uk-Lamda deployment through CICD')
+        'body': json.dumps('uk-Lamda deployment through CICD-GithubChekIn')
     }
