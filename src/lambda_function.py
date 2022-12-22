434d6670ed8ddc917002687a8aeae15b4b506b08
Uk <udayakumar.kolathur@gmail.com>
2022-12-22 10:07:47 -0500

    yes

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -4,5 +4,5 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda123!-PostDeploymentUpdate')
+        'body': json.dumps('uk-Hello from Lambda123!-PostDeploymentUpdate')
     }
