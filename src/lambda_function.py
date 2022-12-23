86b8d406310ae816956fab9028e9c4191174f77c
Uk <udayakumar.kolathur@gmail.com>
2022-12-23 02:14:42 -0500

    code update

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -1,8 +1,12 @@
 import json
+import pandas as pd
 
 def lambda_handler(event, context):
     # TODO implement
+    d = {'col1':[1,2], 'col2':[3,4]}
+    df = pd.DataFrame(data=d)
+    print(df)    
     return {
         'statusCode': 200,
-        'body': json.dumps('Demo Update-uk-Lamda deployment through CICD-GithubChekIn')
+        'body': json.dumps('Demo Update-uk-Lamda deployment through CodePipeline after GithubChekIn')
     }
